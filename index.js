a9998b7ed9470fad0c647669e3ea002b959b659a
rafia02 <rafiabinterashed01@gmail.com>
2023-11-24 22:40:38 +0600

    repleace tips

---
+++ index.js
@@ -0,0 +1,58 @@
+// 1. how to remove falsy value in an array 
+
+// javascript has 6 falsy value. like as, 
+// 0, null, undefined, NaN, "", false
+
+const valueee = ["a", 1, false, {}, [], "hello", undefined, 0, null, "", NaN]
+// just using Boolean funtction 
+// console.log(valueee.filter(Boolean))
+
+
+
+// console.log(typeof({}))
+// console.log(typeof([]))
+// console.log(typeof(NaN))
+// console.log(typeof(null))
+// console.log(typeof(undefined))
+
+
+
+// 2. convert any value to Boolean 
+
+// using !! in font of value 
+// console.log(!!"any value")
+
+
+
+// 3. resizing a array like as crop an array 
+
+// we can use arrays length property 
+const arrayExm = [1, "value", "chcek", 5]
+arrayExm.length = 2
+
+// console.log(arrayExm)
+
+
+// 4. how to flatren multidirection array 
+
+const multyArray = ["rahim", [["korim", "harun", 1, [2, "rinku", 3]], 4], "suma"]
+
+// by using array flat method. but it is flaten by one level array. 
+// if we falten two level array then we can use falt(Infinity)
+
+// console.log(multyArray.flat(Infinity))
+
+
+// 4. replease any word 
+
+const replease = "react a open sorce javascript Farmwork. this farmwork is high. we can use this farmwork for ui"
+
+const newReplese = replease.replace(/farmwork/g, "laibary")
+
+// console.log(newReplese)
+
+
+// 5. perfomence chek korar jonno 
+
+
+

30aa8e63c764356dbf703141f6bd7213d839af44
rafia02 <rafiabinterashed01@gmail.com>
2023-11-28 00:40:25 +0600

    closer

---
+++ index3.js
@@ -0,0 +1,21 @@
+const x = 2
+const y = 3
+function check (){
+ return  function(){
+    return sum = x + y
+ }
+    
+}
+
+
+const nd = check()
+console.log(nd())
+
+
+
+// console.log(x)
+
+// if(true){
+//     var x = 10
+// }
+// console.log(x)
